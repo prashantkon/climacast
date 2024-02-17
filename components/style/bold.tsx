631cc223dd642d1b3ad748c3a15ab830c8313c1e
Rohan Fernandes <59456400+proballstar@users.noreply.github.com>
2024-02-17 13:26:22 -0800

    front page v1

---
+++ components/style/bold.tsx
@@ -0,0 +1,3 @@
+export default function Bold(props: { text: string }) {
+    return <strong className="bold">{props.text}</strong>;
+}
---
+++ src/pages/steveyang.tsx
@@ -0,0 +1,7 @@
+export default function SteveYang() {
+  return (
+    <div className="absolute left-1/2 top-1/2 h-20 w-20 -translate-x-1/2 -translate-y-1/2 transform bg-black">
+      hi
+    </div>
+  );
+}
