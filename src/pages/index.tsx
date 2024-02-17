32afcca7fc2b6fc9316fb57c468566cf73534690
Rohan Fernandes <59456400+proballstar@users.noreply.github.com>
2024-02-17 12:01:30 -0800

    adding clerk/bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
